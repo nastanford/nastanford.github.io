d3187cfe639bfc56a6834834eef8afb7d0c51c97
Nathan Stanford <Nathan.Stanford@gmail.com>
2022-10-03 14:36:33 -0400

    Added Dog Slideshow

--- js/index.js
+++ js/index.js
@@ -1,7 +1,3 @@
-
-// async fetch of file navbar.html to be inserted in the id navbar
-
-
   async function showNavbar() {
     const response = await fetch('includes/navbar.html');
     const navbar = await response.text();
